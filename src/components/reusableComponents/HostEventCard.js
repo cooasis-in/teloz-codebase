136a7cd7d58202fae7ea6f6c32172cf7a8f08230
Shahbaz-cooasis <shahbaz@cooasis.in>
2024-05-31 17:39:32 +0530

    issue-fixed

--- src/components/reusableComponents/HostEventCard.js
+++ src/components/reusableComponents/HostEventCard.js
@@ -59,7 +59,7 @@ const HostEventCard = () => {
   };
 
   return (
-    <div className="container mx-autoborder-b p-10 px-4 md:mt-20 ">
+    <div className="container mx-auto border-b  p-[100px] px-4 md:mt-20">
       <div className="flex flex-col md:flex-row justify-between items-center p-4">
         <h3 className="text-2xl md:text-4xl font-normal text-center md:text-left mb-4 md:mb-0">
           A space to host your next event
